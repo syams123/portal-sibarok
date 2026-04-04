83ebe6011157a4966bb90d39c2867f08a5fc5a14
syams123 <147891702+syams123@users.noreply.github.com>
2026-04-04 21:51:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2033,7 +2033,7 @@ async function konfirmasiBayar() {
 
                 await Swal.fire({
                     title: "Berhasil",
-                    text: `Konfirmasi berhasil! Ustadzah akan segera memverifikasi pembayaran Infaq untuk ${nama}.`,
+                    text: `Konfirmasi berhasil! Ustadzah akan segera memverifikasi pembayaran Infaq untuk ${namaSantriReal}.`,
                     icon: "success",
                     confirmButtonColor: '#198754'
                 });
